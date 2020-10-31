7ec73089d74e015da40cffb1c38b620b1c0101f7
eyal segal <eyal.segal@aidoc.com>
2020-10-31 20:19:49 +0200

    remove prints

--- python_project/python_tester.py
+++ python_project/python_tester.py
@@ -4,21 +4,18 @@ from python_project.test_logger import TestLogger
 
 class PythonJestLogger(TestLogger):
     def describe(self, description, fn):
-        # self.logger.info(description)
-        print(description)
+        self.logger.info(description)
         fn()
 
     def it(self, msg, fn):
         self.describe(f'{msg}', fn)
 
     def _success_writer(self):
-        print("pass")
         self.logger.info("pass")
         return True
 
     def _failure_writer(self):
-        print("fail")
-        # self.logger.error("fail")
+        self.logger.error("fail")
         return False
 
 
